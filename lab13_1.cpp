81edc87b644c74f442c18140666a1c8e064523b3
sriv95 <srivichai95@gmail.com>
2024-12-27 15:01:13 +0700

    lab13_1.cpp complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,32 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum,sumsd,sumgm=A[0],sumhm;
+    for (int i=0;i<N;i++){
+        sum+=A[i];
+    }
+    B[0]=sum/N;
+    
+    for (int i=0;i<N;i++){
+        sumsd+=pow(A[i]-B[0],2);
+    }
+    B[1]=sqrt(sumsd/N);
+    
+    for (int i=1;i<N;i++){
+        sumgm*=A[i];
+    }
+    B[2]=pow(sumgm,1.0/N);
+    
+    for (int i=0;i<N;i++){
+        sumhm+=1/A[i];
+    }
+    B[3]=N/sumhm;
+    
+    B[4]=A[0];B[5]=A[0];
+    for (int i=1;i<N;i++){
+        if(A[i]>B[4]) B[4]=A[i];
+        if(A[i]<B[5]) B[5]=A[i];
+    }
+}
