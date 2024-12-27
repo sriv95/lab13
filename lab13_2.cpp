a697150fa91e12635732dec0da8d741d5b54a2ab
sriv95 <srivichai95@gmail.com>
2024-12-27 16:35:29 +0700

    lab13_2.cpp complete

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,24 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image[N][M]){
+    cout << "------------------------------------------------------------------------\n";
+    for (int i=0;i<N;i++){
+        cout<<"|";
+        for (int j=0;j<M;j++){
+            if(image[i][j]==1) cout<<"*";
+            else cout<<" ";
+        }
+        cout<<"|\n";
+    }
+    cout << "------------------------------------------------------------------------\n";
+}
+
+void updateImage(bool image[N][M],int s,int x,int y){
+    for(int i=x-s+1;i<x+s;i++){
+        for(int j=y-s+1;j<y+s;j++){
+            if(i<N&&j<M&&i>=0&&j>=0&&sqrt(pow(i-x,2)+pow(j-y,2))<=s-1) image[i][j]=1;
+        }
+    }
+}
